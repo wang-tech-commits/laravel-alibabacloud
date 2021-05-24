42a813d38553ff6d6597dc3f569108698d0c4ef4
汪晓雨 <15124532157@163.com>
2021-05-24 15:03:40 +0800

    新增月账单查询

---
+++ src/Alibaba/Billing/MonthlyBill.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MrwangTc\Alibabacloud\Alibaba\Billing;
+
+use AlibabaCloud\SDK\BssOpenApi\V20171214\Models\QueryMonthlyBillRequest;
+use AlibabaCloud\Tea\Exception\TeaUnableRetryError;
+use Carbon\Carbon;
+use Mrwang\Alibabacloud\Exceptions\HttpException;
+use MrwangTc\Alibabacloud\Alibaba\Base;
+
+class MonthlyBill extends Base
+{
+
+    /**
+     * Notes   : 月账单查询
+     * @Date   : 2021/5/24 14:56
+     * @Author : Mr.wang
+     * @param  string  $mounth
+     * @return \AlibabaCloud\SDK\BssOpenApi\V20171214\Models\QueryMonthlyBillResponseBody\data
+     * @throws \Mrwang\Alibabacloud\Exceptions\HttpException
+     */
+    public function QueryMonthlyBill($mounth = '')
+    {
+        $client  = $this->createClient('billing');
+        $request = new QueryMonthlyBillRequest();
+
+        $request->billingCycle = empty($mounth) ? Carbon::now()->format('Y-m') : $mounth;
+
+        $response = $client->QueryMonthlyBill($request);
+        try {
+            $body = $response->body;
+            if ($body->code == 'Success') {
+                return $body->data;
+            }
+        } catch
+        (TeaUnableRetryError $e) {
+            throw new HttpException($e->getMessage());
+        }
+    }
+
+}
